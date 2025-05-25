80607ac6fe51a35e627dfc7f648c384a5ed6309a
Mykyta <nktpedko@gmail.com>
2025-05-25 18:06:23 +0300

    Add application controller

--- src/main/java/com/company/cash/controller/CashController.java
+++ src/main/java/com/company/cash/controller/CashController.java
@@ -1,6 +1,38 @@
 package com.company.cash.controller;
 
+import com.company.cash.model.CashOrder;
+import com.company.cash.service.CashService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/cash")
 public class CashController {
 
+    private final CashService service;
 
+    public CashController(CashService service) {
+        this.service = service;
+    }
+
+    @PostMapping("/order")
+    public String createOrder(@RequestBody CashOrder order) {
+        service.addOrder(order);
+        return "Операцію збережено";
+    }
+
+    @GetMapping("/balance")
+    public Map<String, Object> getBalance() {
+        return Map.of(
+                "balance", service.getBalance(),
+                "currency", "UAH"
+        );
+    }
+
+    @GetMapping("/orders")
+    public List<CashOrder> getAllOrders() {
+        return service.getAllOrders();
+    }
 }
